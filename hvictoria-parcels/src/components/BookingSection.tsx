13db5090bb53814eb40e31cbb955542d3765f0bc
Vincentonguk <vincentonguk@gmail.com>
2025-07-25 00:08:34 +0100

    subindo projeto completo

---
+++ hvictoria-parcels/src/components/BookingSection.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import BookingSection from "@/components/BookingSection";
+import Footer from "@/components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* Outras seções como Hero, About, etc */}
+      <BookingSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
